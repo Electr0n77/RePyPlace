4aafd2053e5c40dff6ed801614187a9b396e137b
Electron <julian@frattarola.net>
2021-02-10 19:34:14 -0500

    Update mainInterface.py

--- mainInterface.py
+++ mainInterface.py
@@ -3,19 +3,86 @@ import replacement
 from replacement import Replacer
 import tkinter as tk
 
-newReplacer = Replacer()
+#newReplacer = Replacer()
 
+#Using colorutils
 buttonColor = Color((110, 190, 250))
 
+#instantiation
 window = tk.Tk()
+window.geometry("400x400")
 
+#strings lolw
+theText = tk.StringVar()
+replacedWord = tk.StringVar()
+replacementWord = tk.StringVar()
+
+#the main fucker
+def Replace():
+	#converting tkinter shit into strings
+	entireText = theWholeText.get()
+	getReplacedWord = replacedWord.get()
+	getReplacementWord = replacementWord.get()
+
+	#replacement
+	newString = entireText.replace(getReplacedWord, getReplacementWord)
+
+	#giving you the jacked replacement
+	theNewText.insert(tk.END, newString)
+
+	#reset, ig
+	replacedWord.set("")
+	replacementWord.set("")
+
+#el button
 theButton = tk.Button(
 	text = "click me",
-	width = 100,
-	height = 100,
+	width = 50,
+	height = 5,
 	bg = buttonColor.web,
-	command = newReplacer.runReplacer()
+	command = Replace
 )
 
+#BUNCHA SHIT
+theLabelOne = tk.Label(window, text="Word to be replaced")
+
+theReplaced = tk.Entry(
+	window,
+	width=100,
+	textvariable = replacedWord
+)
+
+theLabelTwo = tk.Label(window, text="Replacement word")
+
+theReplacement = tk.Entry(
+	window,
+	width=100,
+	textvariable = replacementWord
+)
+
+theLabelThree = tk.Label(window, text="The entire text")
+
+theWholeText = tk.Entry(
+	window,
+	width = 100,
+	textvariable = theText
+)
+
+theLabelFour = tk.Label(window, text="Your new text")
+
+theNewText = tk.Text(
+	height = 4
+)
+
+#OH GOD OH FUCK
 theButton.pack()
+theLabelOne.pack()
+theReplaced.pack()
+theLabelTwo.pack()
+theReplacement.pack()
+theLabelThree.pack()
+theWholeText.pack()
+theButton.pack()
+theLabelFour.pack()
+theNewText.pack()
 window.mainloop()
